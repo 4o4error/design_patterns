fcf2fb34b368c15dcbca4f7e365eb6012c70a54a
Petrescu, Vlad Adrian <vlad.petrescu@siemens.com>
2018-08-20 10:03:13 +0300

    added todo code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,66 @@
 #include <iostream>
 
+
+typedef int volts;
+class USAPhoneCharger;
+class EuropeanSocket;
+class USASocket;
+
+class SocketInterface
+{
+public:
+  virtual int getVoltage() = 0;
+  virtual ~SocketInterface() {}
+};
+
+// starting interface
+class EuropeanSocket : public SocketInterface
+{
+  // Inherited via SocketInterface
+  virtual int getVoltage() override
+  {
+    return 220;
+  }
+};
+
+// target interface 
+class USASocket : public SocketInterface
+{
+public:
+  virtual int getVoltage() override
+  { 
+    return 110; 
+  }
+
+};
+
+// client 
+class USAPhoneCharger
+{
+  SocketInterface* power;
+
+public:
+  void plugIn(SocketInterface* supply)
+  {
+   
+    power =supply ;
+  }
+  void charge()
+  {
+    if (power->getVoltage() > 110)
+    {
+      std::cout << "phone goes boom" << std::endl;
+    }
+    else
+    {
+      std::cout << "phone is fine" << std::endl;
+    }
+  }
+};
 void main (){
-  std::cout << "Hello Design Patterns"<<std::endl;
+  //TODO: charge the phone with an european socket without blowing it up
+  SocketInterface* socket = new EuropeanSocket;
+  USAPhoneCharger phoneCharger;
+  phoneCharger.plugIn(socket);
+  phoneCharger.charge();
 }
